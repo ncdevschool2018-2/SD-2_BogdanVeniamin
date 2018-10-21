e25967c7ef15da3e3fd82e1072033e5b998b6da8
VenyaBohdan <venya.bohdan@gmail.com>
2018-10-22 01:16:07 +0300

    Still not the end

---
+++ sd4parent/frontend/card.js
@@ -0,0 +1,20 @@
+window.onload  = function() {
+  var modal = document.getElementById("myModal")
+  var btn = document.getElementById("myBtn")
+  var span = document.getElementsByClassName("end")[0]
+
+  btn.onclick = function() {
+    modal.style.display = "block"
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none"
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+}
---
+++ sd4parent/frontend/login.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  var modal = document.getElementById("myModal")
+  var btn = document.getElementById("myBtn")
+
+  btn.onclick = function() {
+    modal.style.display = "block"
+  }
+
+  window.onclick = function(event) {
+    if(event.target == modal) {
+      modal.style.display = "none"
+    }
+  }
+}
